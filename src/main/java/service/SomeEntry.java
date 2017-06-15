1ac79dc69105071169c504fdb01302a3a39ebbd6
Vasiliy Komarov <nexsus12@mail.ru>
2017-06-16 00:51:05 +0300

    1

--- src/main/java/service/SomeEntry.java
+++ src/main/java/service/SomeEntry.java
@@ -2,10 +2,23 @@ package service;
 
 public class SomeEntry {
 
+    private String _key;
+    private byte[] _value;
 
     public String getKey() {
-        return "";
+        return _key;
     }
 
+    public byte[] getValue() {
+        return _value;
+    }
+
+    public void setKey(String key) {
+        _key = key;
+    }
+
+    public void setValue(byte[] value) {
+        _value = value;
+    }
 
 }
