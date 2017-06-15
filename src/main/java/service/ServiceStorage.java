6ed31ad689e1c0d1b15d14919e435dfd12928967
Vasiliy Komarov <nexsus12@mail.ru>
2017-06-16 00:35:52 +0300

    1

--- src/main/java/service/ServiceStorage.java
+++ src/main/java/service/ServiceStorage.java
@@ -8,26 +8,29 @@ import static java.util.Optional.ofNullable;
 
 public class ServiceStorage implements Service {
 
-    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-    private final Lock readLock = readWriteLock.readLock();
-    private final Lock writeLock = readWriteLock.writeLock();
+//    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+//    private final Lock readLock = readWriteLock.readLock();
+//    private final Lock writeLock = readWriteLock.writeLock();
 
     public byte[] get(String key) throws WrongKeyException, Exception {
-        String newKey = ofNullable(key).map(String::trim).filter(s -> !s.isEmpty() && s.length() < 129).orElseThrow(WrongKeyException::new);
+        String newKey = ofNullable(key)
+                .map(String::trim)
+                .filter(s -> !s.isEmpty() && s.length() < 129)
+                .orElseThrow(WrongKeyException::new);
 
-        readLock.lock();
+//        readLock.lock();
         try {
             int hashCode = newKey.hashCode();
         } catch (Exception e) {
             throw e;
         } finally {
-            readLock.unlock();
+//            readLock.unlock();
         }
         return new byte[1];
     }
 
     public void put(String key, byte[] data) throws WrongKeyException, Exception {
-        writeLock.lock();
+//        writeLock.lock();
 
         try {
             int hashCode = key.hashCode();
@@ -35,12 +38,12 @@ public class ServiceStorage implements Service {
         } catch (Exception e) {
 
         } finally {
-            writeLock.unlock();
+//            writeLock.unlock();
         }
     }
 
     public void remove(String key) throws WrongKeyException, Exception {
-        writeLock.lock();
+//        writeLock.lock();
 
         try {
             int hashCode = key.hashCode();
@@ -48,7 +51,7 @@ public class ServiceStorage implements Service {
         } catch (Exception e) {
 
         } finally {
-            writeLock.unlock();
+//            writeLock.unlock();
         }
     }
 }
